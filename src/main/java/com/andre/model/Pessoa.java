b36ee98a09ed6dd54157e34b299c1ac36efede89
André Roberto <andre.robertolins2018@gmail.com>
2020-11-19 18:54:37 -0300

    Enviando projeto para integração com Jenkins

---
+++ src/main/java/com/andre/model/Pessoa.java
@@ -0,0 +1,27 @@
+package com.andre.model;
+
+public class Pessoa {
+	
+	private String nome;
+	private int CPF;
+	public Pessoa(String nome, int cPF) {
+		super();
+		this.nome = nome;
+		CPF = cPF;
+	}
+	public String getNome() {
+		return nome;
+	}
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	public int getCPF() {
+		return CPF;
+	}
+	public void setCPF(int cPF) {
+		CPF = cPF;
+	}
+	
+	
+
+}
