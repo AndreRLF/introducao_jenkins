f33e662c155cea7e51e1d8c6e9797f7b17957a6e
André Roberto <andre.robertolins2018@gmail.com>
2020-11-24 21:43:19 -0300

    Testando Jenkins

--- src/main/java/com/andre/model/Pessoa.java
+++ src/main/java/com/andre/model/Pessoa.java
@@ -4,24 +4,27 @@ public class Pessoa {
 	
 	private String nome;
 	private int CPF;
+	
 	public Pessoa(String nome, int cPF) {
 		super();
 		this.nome = nome;
 		CPF = cPF;
 	}
+	
 	public String getNome() {
 		return nome;
 	}
+	
 	public void setNome(String nome) {
 		this.nome = nome;
 	}
+	
 	public int getCPF() {
 		return CPF;
 	}
+	
 	public void setCPF(int cPF) {
 		CPF = cPF;
 	}
 	
-	
-
 }
